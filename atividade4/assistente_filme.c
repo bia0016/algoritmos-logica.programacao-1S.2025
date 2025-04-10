131621fe32a77031c89d2c05ade8af2fad740ce3
bia0016 <biaevangelistamoreira@gmail.com>
2025-04-10 11:14:05 -0300

    assistente filmes

--- atividade4/assistente_filme.c
+++ atividade4/assistente_filme.c
@@ -17,6 +17,8 @@ int main(int argc, char* argv[]){
             printf("Filmes Leves> indicacao de Filme de Animacao: TOY STORY.\n");
         } else if(opcao == 'c'){
             printf("Filmes Leves> indicacao de Filme de Comedia: TROVAO TROPICAL.\n");
+        } else {
+            printf("Opcao invalida.\n");
         }
     } else if(opcao == 'i'){
         printf("Assistir Filmes Intensos.\n");
@@ -34,6 +36,8 @@ int main(int argc, char* argv[]){
                 printf("Filmes Intensos> Terror> indicacao para Filme de Terror Psicologico: CORRA!.\n");
             }else if(opcao == 'n'){
                 printf("Filmes intensos> Terror> indicacao para Filme de Terror Natural: INVOCACAO DO MAL.\n");
+            } else {
+                printf("Opcao invalida.\n");
             }
         }else if(opcao == 's'){
             printf("Filmes Intensos de Acao.\n");
@@ -45,10 +49,15 @@ int main(int argc, char* argv[]){
                 
             }else if(opcao == 'h'){
             printf("Filmes Intensos> Acao> indicacao para Filme de Acao de Super-Heroi: HOMEM-ARANHA.\n");
-           
+        } else{
+            printf("Opcao invalida.\n");
         }
+        } else{
+            printf("Opcao invalida.\n");
         }
    
+    } else{
+        printf("Opcao invalida.\n");
     }
     return 0;
 }
