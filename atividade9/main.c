072b1e19034997988c6cc83c87f158cc1ac7acb1
bia0016 <biaevangelistamoreira@gmail.com>
2025-06-13 11:00:33 -0300

    arquivos

--- atividade9/main.c
+++ atividade9/main.c
@@ -1,8 +1,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-//argv[2], [3], [4]
+#include <time.h>
+
 int main(int argc, char* argv[]){
     if(argc != 5){
         printf("eh necessario pelo menos 5 argumentos\n");
@@ -17,17 +17,23 @@ int main(int argc, char* argv[]){
     int a = atoi(argv[2]);
     int z = atoi(argv[3]);
     int valesp = atoi(argv[4]);
-    //a e valesp devem ser menores que z
+
     if(a > z){
         printf("primeiro argumento deve ser menor que o segundo\n");
         return 1;
     }
-    if(valesp >= z){
+    if(valesp > z){
         printf("o valor esperado deve ser menor ou igual ao segundo argumento\n");
         return 1;
     }
+    if(valesp < a){
+        printf("o valor esperado deve ser maior ou igual ao primeiro argumento\n");
+        return 1;
+    }
+    srand(time(NULL));
     int sorteado = rand() % (z - a + 1) + a;
-    fprintf(log, "primeiro numero = %d\nultimo numero = %d\nvalor esperado = %d\n valor sorteado = %d\n", a, z, valesp, sorteado);
+    printf("primeiro numero = %d\nultimo numero = %d\nvalor esperado = %d\nvalor sorteado = %d\n", a, z, valesp, sorteado);
+    fprintf(log, "primeiro numero = %d\nultimo numero = %d\nvalor esperado = %d\nvalor sorteado = %d\n", a, z, valesp, sorteado);
     fclose(log);
     return 0;
 }
